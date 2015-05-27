0c1f07c21917b0b424d276516fbecbbd71382f2e
lzhengms <lzhengms@gmail.com>
2015-05-27 18:40:12 +0800

    modify index

--- index.js
+++ index.js
@@ -5,4 +5,45 @@
 
 'use strict';
 
-module.exports = require('./vendor/ZeroClipboard');
+var ZeroClipBoard = require('./vendor/ZeroClipboard');
+var widget = require('nd-widget');
+var Alert = require('nd-alert');
+
+var ClipBoard = module.exports = widget.extend({
+  attrs: {
+    trigger: null,
+    swf: null,
+    getCopyText:function(target){
+      return '';
+    }
+  },
+  setup: function () {
+    ClipBoard.superclass.setup.call(this);
+
+    ZeroClipBoard.config({swfPath: this.get('swf')});
+    var that=this,
+      client = new ZeroClipBoard(this.get('trigger'));
+
+    client.on('ready', function (event) {
+
+      client.on('copy', function (event) {
+        event.clipboardData.clearData();
+        event.clipboardData.setData('text/plain', that.get('getCopyText').call(that,event.target));
+      });
+
+      client.on('aftercopy', function (event) {
+        Alert.show('复制成功');
+      });
+
+    });
+
+    client.on('error', function (event) {
+      ZeroClipboard.destroy();
+    });
+
+  }
+
+});
+
+
+
